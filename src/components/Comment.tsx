9e4609c7c9dd9fe05d4e137bc960ee3ad822f541
MARCELO RODRIGUES <marcelorodriguesweb@gmail.com>
2022-11-06 16:36:20 -0300

    first-commit

---
+++ src/components/Comment.tsx
@@ -0,0 +1,52 @@
+import styles from './Comment.module.css';
+
+import { Trash, ThumbsUp } from 'phosphor-react';
+import { Avatar } from './Avatar';
+import { useState } from 'react';
+
+interface CommentProps {
+    content: string;
+    onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment() {
+        console.log('deletar');
+        onDeleteComment(content);
+    }
+    function handleLikeComment() {
+        setLikeCount( state => {
+            return state + 1;
+        })
+    }
+
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/MRodrigues51.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Marcelo Rodrigues</strong>
+                            <time title='28 de Outubro às 16:14' dateTime='2022-10-28 08:13:30'>Cerca de 1h atrás</time>
+                        </div>
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
+                            <Trash size={24}/>
+                        </button>
+                    </header>
+                    <p>{content}</p>
+                </div>
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    );
+}
